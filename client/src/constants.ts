f90f40da181f41ddec78915fa6ce31775c803c7a
bal7hazar <bal7hazar@proton.me>
2025-09-26 11:54:30 +0200

    fix: items page

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,5 +1,6 @@
 // Constants for the project
 export const DEFAULT_PROJECT: string = "arcade-main";
+export const DEFAULT_PRESET: string = "arcade";
 export const DEFAULT_TOKENS_PROJECT: string = "c7e-arcade-tokens";
 // Constants required to query the achievement events
 export const TROPHY: string = "TrophyCreation";
