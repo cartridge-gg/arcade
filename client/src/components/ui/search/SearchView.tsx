0364ca2cb993ca0ecd18faca27798d2458cd7d3e
MartianGreed <valentin@pupucecorp.com>
2025-10-21 11:16:07 +0200

    feat(navigation): header re-vamp

---
+++ client/src/components/ui/search/SearchView.tsx
@@ -0,0 +1,20 @@
+import type { SearchViewModel } from "@/features/search/useSearchViewModel";
+import { SearchInput } from "@/components/ui/modules/search-input";
+
+export const SearchView = ({
+  disabled,
+  searchValue,
+  placeholder,
+  onSearchChange,
+}: SearchViewModel) => {
+  return (
+    <div className="w-1/6">
+      <SearchInput
+        value={searchValue}
+        onChange={onSearchChange}
+        placeholder={placeholder}
+        disabled={disabled}
+      />
+    </div>
+  );
+};
