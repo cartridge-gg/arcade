ef2c4ee083edde1c2f812317ff5e0c98f0cc35a5
MartianGreed <valentin@pupucecorp.com>
2025-10-23 09:53:17 +0200

    fix(navigation): router matching

--- client/src/routes/game/$game.tsx
+++ client/src/routes/game/$game.tsx
@@ -1,6 +1,5 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { MarketplaceScene } from "@/components/scenes/marketplace";
+import { createFileRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/game/$game")({
-  component: MarketplaceScene,
+  component: Outlet,
 });
