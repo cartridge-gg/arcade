64ecae6899ba8815358bfb3b2702f74cc3682a54
nrmnqdds <quddusnuriman@gmail.com>
2025-12-09 22:37:26 +0800

    style: update footer structure

--- client/src/components/ui/marketplace/collectible-card/footer.tsx
+++ client/src/components/ui/marketplace/collectible-card/footer.tsx
@@ -10,7 +10,7 @@ export interface CollectibleCardFooterProps
 }
 
 const collectibleCardFooterVariants = cva(
-  "absolute bottom-0 w-full px-3 py-2 flex flex-row items-center justify-between gap-0.5 text-foreground-400 data-[hidden=true]:hidden",
+  "absolute bottom-0 w-full p-3 flex flex-col gap-1 text-foreground-400 data-[hidden=true]:hidden",
   {
     variants: {
       variant: {
@@ -31,31 +31,26 @@ export function CollectibleCardFooter({
   className,
   ...props
 }: CollectibleCardFooterProps) {
-  console.log("price: ", price);
   return (
     <div
       data-hidden={price === undefined && lastSale === undefined}
       className={cn(collectibleCardFooterVariants({ variant }), className)}
       {...props}
     >
-      <div className="flex flex-col items-start gap-1">
-        <p className="text-foreground-300 text-[10px]/3 font-normal">Price</p>
+      <div className="flex items-center justify-between gap-1 text-foreground-300 text-[10px]/3 font-normal">
+        <p>Price</p>
+        <p>Last Sale</p>
+      </div>
+      <div className="flex items-center justify-between text-foreground-100 text-sm font-medium">
         {!!price && typeof price === "string" ? (
-          <p className="text-foreground-100 text-sm font-medium">
-            {Number(price).toFixed(2)}
-          </p>
+          <p className="">{Number(price).toFixed(2)}</p>
         ) : !!price && typeof price === "object" ? (
           <Price price={price} />
         ) : (
           <p>--</p>
         )}
-      </div>
-      <div className="flex flex-col items-end gap-1">
-        <p className="text-foreground-300 text-[10px]/3 font-normal">
-          Last Sale
-        </p>
         {!!lastSale && typeof lastSale === "string" ? (
-          <p className="text-foreground-100 text-sm font-medium">{lastSale}</p>
+          <p>{lastSale}</p>
         ) : !!lastSale && typeof lastSale === "object" ? (
           <Price price={lastSale} />
         ) : (
@@ -76,9 +71,7 @@ function Price({ price }: { price: { value: string; image: string } }) {
         rounded
         transdark
       />
-      <p className="text-foreground-100 text-sm font-medium">
-        {Number(price.value).toFixed(2)}
-      </p>
+      <p>{Number(price.value).toFixed(2)}</p>
     </div>
   );
 }
