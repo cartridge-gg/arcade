5a937118449e02cf6df3b8c2023b543e144c1735
nrmnqdds <quddusnuriman@gmail.com>
2026-01-29 23:44:36 +0800

    fix: sidebar gap and restore content border

--- client/src/components/base-template.tsx
+++ client/src/components/base-template.tsx
@@ -10,6 +10,7 @@ import {
   ArcadeIcon,
   ArcadeIconText,
 } from "@/components/ui/modules/arcade-header";
+import { useAccount } from "@starknet-react/core";
 
 interface BaseTemplateProps {
   children: ReactNode;
@@ -32,6 +33,7 @@ export function BaseTemplate({
   const { isOpen, handleTouchMove, handleTouchStart } = useSidebar();
 
   const { isMobile } = useDevice();
+  const { account } = useAccount();
 
   return (
     <>
@@ -59,7 +61,7 @@ export function BaseTemplate({
 
             <div
               className={cn(
-                "h-full flex flex-col",
+                "h-full flex flex-col lg:gap-4",
                 "fixed lg:relative left-0 w-[min(calc(100vw-64px),360px)]",
                 "transition-all duration-300 ease-in-out",
                 !isOpen && isMobile ? "-translate-x-full" : "translate-x-0",
@@ -69,7 +71,11 @@ export function BaseTemplate({
             >
               <Link
                 to="/"
-                className="lg:hidden p-4 bg-background-100 border-r border-r-spacer-100 border-b border-b-background-200 flex items-center justify-start gap-2 text-primary w-auto h-[66px]"
+                className={cn(
+                  "lg:hidden p-4 bg-background-100 border-r border-r-spacer-100 flex items-center justify-start gap-2 text-primary w-auto h-[66px]",
+                  "border-b",
+                  account ? "border-b-spacer-100" : "border-b-background-200",
+                )}
               >
                 <ArcadeIcon className="h-5" />
                 <ArcadeIconText className="h-5" />
@@ -97,7 +103,7 @@ export function BaseTemplate({
 
               <div
                 className={cn(
-                  "relative grow h-full flex flex-col min-w-0 rounded-none lg:rounded-xl lg:gap-3 overflow-hidden bg-background-100 p-3 lg:p-6 order-2 lg:order-3",
+                  "relative grow h-full flex flex-col min-w-0 rounded-none lg:rounded-xl lg:gap-3 overflow-hidden border border-background-200 bg-background-100 p-3 lg:p-6 order-2 lg:order-3",
                   contentClassName,
                 )}
               >
